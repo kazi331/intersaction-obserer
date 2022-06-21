42a761a862db5e88becb500996c11abff9c11515
kazi331 <kazisharifulislam52@gmail.com>
2022-06-22 00:47:01 +0600

    replaced src portion

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const images = document.querySelectorAll("img");
 
 
 // interseaction observer 
-
 const imageOptions = {};
 
 // const observer = new IntersectionObserver(callback, options);
 const imageObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-        if(!entry.isIntersecting) return;
+        if (!entry.isIntersecting) return;
         const image = entry.target;
-        console.log(image);
-        const newUrl = image.getAttribute('data-src');
-        image.src = newUrl;
+        image.src = image.src.replace("w=10", "w=2000")
+        // const newUrl = image.getAttribute('data-src');
+        // image.src = newUrl;
         // stop observer 
         observer.unobserve(image);
     })
 }, imageOptions);
-    
+
 images.forEach(image => {
     // const newUrl = image.getAttribute('data-src');
     // image.src = newUrl;
